8affea0a9a3f4819fb76f36d7bc61ea69fe94993
Xelame <rolland.alexandre2@gmail.com>
2023-06-16 11:54:40 +0200

    go back and finish

--- connexion.php
+++ connexion.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
-var_dump($_ENV);
 // Connexion à la base de données
-$servername = $_ENV['SERVERNAME'];
-$port = $_ENV['PORT'];
-$username = $_ENV['USERNAME'];
-$password = $_ENV['PASSWORD'];
+$servername = "2a01:e0a:46a:2780:545c:98ff:fe16:72cc";
+$port = "3310";
+$username = "root";
+$password = "7Dk7SQneHt39a6";
 $dbname = "quiz";
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
